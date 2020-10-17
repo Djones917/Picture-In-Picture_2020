35df562ff70bf35dafdb20b04ab9077cbb24ebd4
Derek <nightwing917@hotmail.com>
2020-10-16 19:28:42 -0600

    might run into error with requestPictureInPicture

--- script.js
+++ script.js
@@ -6,11 +6,22 @@ const button = document.getElementById('button');
 async function selectMediaStream() {
     try {
       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+        videoElement.play();
+      }
     } catch (error) {
       console.log('Whoops, error here:', error);
     }
 }
 
+button.addEventListener('click', asyn () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Pictue In Picture
+  await videoElement.requestPictureInPictue();
+
+});
 
 // On Load
 selectMediaStream();
