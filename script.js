be82b667a5e9cfb9f68a04e0b4f5785839b74ffa
Derek <nightwing917@hotmail.com>
2020-10-16 19:04:19 -0600

    added medaiStream

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const button = document.getElementById('button');
 
 
 // Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    } catch (error) {
+      console.log('Whoops, error here:', error);
+    }
+}
+
+
+// On Load
+selectMediaStream();
